8323d55a8830ca59340afeb62b1fa3e84de07d4a
晟昊 任 <Rsh0111@126.com>
2023-03-24 15:21:36 +0800

    fix my MorningCard and deploy it by gitlab

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
     要想实现此仓库功能需要修改少量代码，若未曾了解过Python请不要复刻仓库
 
-
-
 ![墨菲安全](https://s.murphysec.com/badge/fromann/DailyWechat.svg)
 [本项目已通过**墨菲安全**检测](https://www.murphysec.com/p/fromann/DailyWechat)
 
@@ -23,13 +21,15 @@
 
 ## 正文
 ### 效果
-![1](https://raw.githubusercontent.com/fromann/CDN/main/img/githubpic/sendcard/1.png)
+<img src="https://raw.githubusercontent.com/wjrzm/PicGO/main/2022/202303241455930.jpg" style="zoom:50%;" />
+
 ### 思路
 本项目实现单公众号对多用户发送模板信息
-#### Secrets/Action Key表
+#### 使用GitHub Action进行部署
+##### Secrets/Action Key表
 将公众号的`APP_ID` , `APP_SECRET` , `Template_ID`填入Key表
 ![2](https://raw.githubusercontent.com/fromann/CDN/main/img/githubpic/sendcard/2.png)
-#### 用户信息文件
+##### 用户信息文件
 用户信息文件储存于json文件中，实现用户信息的差异化储存，便于差异化分发
 
     开源平台也要保护好自己的名字资料
@@ -39,14 +39,20 @@
     {
       "user_name": "用户1的名字",
       "user_id": "用户1的ID",
-      "born_date": "用户1的出生日期（注意格式）",
-      "city": "用户1城市"
+      "born_date": "用户1设置的起始日，已经过去的节日",
+      "birthday": "用户1的生日",
+      "setday": "用户1设置的倒数日，未来计划的节日",
+      "city": "城市名",
+      "district": "区名"
     },
     {
       "user_name": "用户2的名字",
       "user_id": "用户2的ID",
-      "born_date": "用户2的出生日期（注意格式）",
-      "city": "用户2的城市" 
+      "born_date": "用户2设置的起始日，已经过去的节日",
+      "birthday": "用户2的生日",
+      "setday": "用户2设置的倒数日，未来计划的节日",
+      "city": "城市名",
+      "district": "区名"
     }
     ]
 }
@@ -55,28 +61,63 @@
 若想新增用户可以按照以下格式花括号（`{}`）之间添加用`,`分割
 ~~~json
 {
-  "user_name": "用户2的名字",
-  "user_id": "用户2的ID",
-  "born_date": "用户2的出生日期（注意格式）",
-  "city": "用户2的城市" 
+      "user_name": "用户2的名字",
+      "user_id": "用户2的ID",
+      "born_date": "用户2设置的起始日，已经过去的节日",
+      "birthday": "用户2的生日",
+      "setday": "用户2设置的倒数日，未来计划的节日",
+      "city": "城市名",
+      "district": "区名"
 }
 ~~~
+
+#### 使用GitLab CI/CD进行部署
+
+由于GitHub相较于GitLab偏重于开源代码的共享，不适合进行一些个人信息以及用户密钥等信息的设置，所以计划用GitLab个人仓库进行部署。
+
+##### 首先进入CI/CD中的编辑器
+
+```yml
+image: python:3.7
+
+stages:          # List of stages for jobs, and their order of execution
+  - build
+  - deploy
+
+build-job:       # This job runs in the build stage, which runs first.
+  stage: build
+  script:
+    - echo "Compiling the code..."
+    - echo "Compile complete."
+
+deploy-job:      # This job runs in the deploy stage.
+  stage: deploy  # It only runs when *both* jobs in the test stage complete successfully.
+  environment: production
+  script:
+    - echo "Deploying application..."
+    - echo "Application successfully deployed."
+    - pip install -r ./requirements.txt && python ./main.py
+```
+
+然后进入`计划`模块，进行流水线计划的设定，循环周期`0 9 * * *`，第一位表示分钟，第二位表示小时，以此类推，此例表示每天上午九点进行流水线的执行，即`MoriningCard`的推送。
+
+这样可以在配置文件中直接设置任何你想要的API密钥，包括一些很私密的个人信息。
+
 #### 微信模板
 
 ~~~txt
 {{time.DATA}}
 
-φ(゜▽゜*)♪{{name.DATA}}小盆友
+坐标城市：{{city.DATA}}
+当前天气：{{weather.DATA}}
+当前风向：{{wind.DATA}}
+今日温度：{{tem_low.DATA}}℃~{{tem_high.DATA}}℃ 
+空气质量：{{air.DATA}}
+紫外线强度：{{uv.DATA}}级
 
-坐标城市：{{city.DATA}}o(〃'▽'〃)o
-当前天气：{{weather.DATA}} ヾ(≧▽≦*)o
-当前风向：{{wind.DATA}}( •̀ ω •́ )✧
-今日温度：{{tem_low.DATA}}℃~{{tem_high.DATA}}℃ ( •̀ ω •́ )
-空气质量：{{air.DATA}}(✿◡‿◡)
-紫外线强度：{{uv.DATA}}(oﾟvﾟ)ノ
-
-庆祝自己在世界上第{{born_days.DATA}}天(❁´◡`❁)
-距离下次生日还有{{birthday_left.DATA}}天╰(*°▽°*)╯
+今天是我们恋爱第{{born_days.DATA}}天
+距离宝贝生日还有{{birthday_left.DATA}}天
+距离五一出游还有{{setday_left.DATA}}天
 
 {{words.DATA}}
 ~~~
@@ -84,3 +125,13 @@
 
 - 生日的日期格式是：`05-20`，
 - 纪念日的格式是 `2022-08-09`
+- 由于个人微信公众号的个人认证需要粉丝量500以上，才可以使用模板分发的功能（本项目主要使用模板消息功能），所以个人用户可以在`开发者工具`->`公众平台测试账号`实现本功能
+
+## 声明变更
+
+- 修改`.github\workflows\main.yml`中的设定时间
+- 修改`main.py`中的天气调用API，和密钥管理方式
+- 修改`users_info.json`格式
+- 修改README，新增解释
+- 新增`Template.txt`
+- 新增`.gitlab-ci.yml`
--- main.py
+++ main.py
@@ -6,18 +6,27 @@ import os
 import random
 import json
 
-nowtime = datetime.utcnow() + timedelta(hours=8)  
+nowtime = datetime.utcnow() + timedelta(hours=8)
 today = datetime.strptime(str(nowtime.date()), "%Y-%m-%d")
 
-app_id = os.getenv("APP_ID")
-app_secret = os.getenv("APP_SECRET")
-template_id = os.getenv("TEMPLATE_ID")
+# 如果你想部署在GitHub Actions上，可以使用下面的代码
+# app_id = os.getenv("APP_ID")
+# app_secret = os.getenv("APP_SECRET")
+# template_id = os.getenv("TEMPLATE_ID")
+
+# 如果你想部署在GitLab上，可以使用下面的代码
+app_id = "YOUR_APP_ID"
+app_secret = "YOUR_APP_SECRET"
+template_id = "YOUR_TEMPLATE_ID"
+weatherApiKey = "YOUR_WEATHER_API_KEY"
+
 
 def get_time():
     dictDate = {'Monday': '星期一', 'Tuesday': '星期二', 'Wednesday': '星期三', 'Thursday': '星期四',
                 'Friday': '星期五', 'Saturday': '星期六', 'Sunday': '星期天'}
     a = dictDate[nowtime.strftime('%A')]
-    return nowtime.strftime("%Y年%m月%d日 %H时%M分 ")+ a
+    return nowtime.strftime("%Y年%m月%d日 %H时%M分 ") + a
+
 
 def get_words():
     words = requests.get("https://api.shadiao.pro/chp")
@@ -25,94 +34,125 @@ def get_words():
         return get_words()
     return words.json()['data']['text']
 
+
 def get_random_color():
     return "#%06x" % random.randint(0, 0xFFFFFF)
 
-def get_weather(city):
-    url = "https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&city=" + city
-    res = requests.get(url).json()
-    weather = res['data'][0]
-    return weather
+# 天气API使用https://dev.qweather.com/的API
+
+
+def get_geo(district, city):
+    urlGeo = "https://geoapi.qweather.com/v2/city/lookup?location=" + \
+        district + "&adm=" + city + "&key=" + weatherApiKey
+    location = requests.get(urlGeo).json()
+    return location["location"][0]["id"]
+
+
+def get_weather(district, city):
+    urlWeather = "https://devapi.qweather.com/v7/weather/3d?location=" + \
+        get_geo(district, city) + "&key=" + weatherApiKey
+    weather = requests.get(urlWeather).json()
+
+    return weather["daily"][0]
+
+
+def get_air(district, city):
+    urlAir = "https://devapi.qweather.com/v7/air/now?location=" + \
+        get_geo(district, city) + "&key=" + weatherApiKey
+    air = requests.get(urlAir).json()
+
+    return air["now"]
+
 
 def get_count(born_date):
     delta = today - datetime.strptime(born_date, "%Y-%m-%d")
-    return delta.days
+    return delta.days + 1
 
 
-def get_birthday(birthday):
-    nextdate = datetime.strptime(str(today.year) + "-" + birthday, "%Y-%m-%d")
+def get_leftday(setday):
+    nextdate = datetime.strptime(str(today.year) + "-" + setday, "%Y-%m-%d")
     if nextdate < today:
         nextdate = nextdate.replace(year=nextdate.year + 1)
     return (nextdate - today).days
 
-client = WeChatClient(app_id, app_secret)
-wm = WeChatMessage(client)
 
-f = open("./users_info.json", encoding="utf-8")
-js_text = json.load(f)
-f.close()
-data = js_text['data']
-num = 0
-for user_info in data:
-    born_date = user_info['born_date']
-    birthday = born_date[5:]
-    city = user_info['city']
-    user_id = user_info['user_id']
-    name=user_info['user_name'].upper()
-    
-    weather= get_weather(city)
+if __name__ == '__main__':
+    client = WeChatClient(app_id, app_secret)
+    wm = WeChatMessage(client)
 
-    data = dict()
-    data['time'] = {
-        'value': get_time(), 
-        'color':'#470024'
+    f = open("./users_info.json", encoding="utf-8")
+    js_text = json.load(f)
+    f.close()
+    data = js_text['data']
+    num = 0
+    for user_info in data:
+        born_date = user_info['born_date']
+        birthday = user_info['birthday'][5:]
+        setday = user_info['setday'][5:]
+        city = user_info['city']
+        district = user_info['district']
+        user_id = user_info['user_id']
+        # name=user_info['user_name'].upper()
+
+        weather = get_weather(district, city)
+        air = get_air(district, city)
+
+        data = dict()
+        data['time'] = {
+            'value': get_time(),
+            'color': '#470024'
         }
-    data['words'] = {
-        'value': get_words(), 
-        'color': get_random_color()
+        data['words'] = {
+            'value': get_words(),
+            'color': get_random_color()
         }
-    data['weather'] = {
-        'value': weather['wea'], 
-        'color': '#002fa4'
+        data['weather'] = {
+            'value': weather['textDay'],
+            'color': '#002fa4'
         }
-    data['city'] = {
-        'value': city, 
-        'color': get_random_color()
+        data['city'] = {
+            'value': city,
+            'color': get_random_color()
         }
-    data['tem_high'] = {
-        'value': weather['tem1'], 
-        'color': '#D44848'
+        data['tem_high'] = {
+            'value': weather['tempMax'],
+            'color': '#D44848'
         }
-    data['tem_low'] = {
-        'value': weather['tem2'], 
-        'color': '#01847F'
+        data['tem_low'] = {
+            'value': weather['tempMin'],
+            'color': '#01847F'
         }
-    data['born_days'] = {
-        'value': get_count(born_date), 
-        'color': get_random_color()
+        data['born_days'] = {
+            'value': get_count(born_date),
+            'color': get_random_color()
         }
-    data['birthday_left'] = {
-        'value': get_birthday(birthday), 
-        'color': get_random_color()
+        data['birthday_left'] = {
+            'value': get_leftday(birthday),
+            'color': get_random_color()
         }
-    data['air'] = {
-        'value': weather['air_level'], 
-        'color': get_random_color()
+        data['setday_left'] = {
+            'value': get_leftday(setday),
+            'color': get_random_color()
         }
-    data['wind'] = {
-        'value': weather['win'][0], 
-        'color': get_random_color()
+        data['air'] = {
+            'value': air['category'],
+            # 'value': "good",
+            'color': get_random_color()
         }
-    data['name'] = {
-        'value': name, 
-        'color': get_random_color()
+        data['wind'] = {
+            'value': weather['windDirDay'],
+            'color': get_random_color()
         }
-    data['uv'] = {
-        'value': weather['uvDescription'], 
-        'color': get_random_color()
+        # data['name'] = {
+        #     'value': name,
+        #     'color': get_random_color()
+        #     }
+        data['uv'] = {
+            'value': weather['uvIndex'],
+            'color': get_random_color()
         }
-    
-    res = wm.send_template(user_id, template_id, data)
-    print(res)
-    num += 1
-print(num)
+
+        res = wm.send_template(user_id, template_id, data)
+        print(res)
+        num += 1
+    print("已发送", num, "条消息")
